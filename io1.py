4c64b5260e188659889d08fd51bb383b1d817e4a
ladnjay <ladnjay@gmail.com>
2020-03-21 14:38:56 +0530

    io update three

--- io1.py
+++ io1.py
@@ -1,4 +1,17 @@
 userName = input('Enter your name: ')
+userAge = input('Enter your age: ')
+userFac = input('Enter a number: ')
+#userFac = 5
 
-print('Hello ', userName, '!', sep='')
+finalName = userName + '!'
 
+inAge = int(userAge) + int(userFac)
+devAge = int(userAge) / int(userFac)
+mulAge = int(userAge) * int(userFac)
+
+
+print('Hello', finalName)
+
+print('In', userFac, 'years you will be', inAge, 'years old')
+print('Your age devided by', userFac, 'is', devAge)
+print('Your age multiplied by', userFac, 'is', mulAge)
