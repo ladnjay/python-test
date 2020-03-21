7371a0f3148edd44322fd0a04570e377526bb322
ladnjay <ladnjay@gmail.com>
2020-03-21 21:22:58 +0530

    third list

--- list1.py
+++ list1.py
@@ -3,3 +3,5 @@ names = ["kamal", "jagath", "sunil", "ajith", "nimal"]
 for index in range(0,len(names)):
     print(names[index],'is stored at index', index)
 
+for name in names:
+    print(name, 'is stored at index', names.index(name))
