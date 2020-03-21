074bf1fb1e22ab4e978c775652610d78b47b7680
ladnjay <ladnjay@gmail.com>
2020-03-21 13:49:42 +0530

    second commit

---
+++ variables.py
@@ -0,0 +1,24 @@
+varString = "The " \
+            "String"
+
+varInteger = 64
+
+varLong = 65565
+
+varFloat = 999.99
+
+varList = [1, 2, 3, 4.56, "Alex", "KIM"]
+
+varTuple = (7, 8.90, "Jane", "SIX")
+
+varDictionary = {'First': 'First item',
+                 'Second': 'Second item'}
+
+
+print(varString)
+print(varInteger)
+print(varLong)
+print(varFloat)
+print(varList)
+print(varTuple)
+print(varDictionary)
